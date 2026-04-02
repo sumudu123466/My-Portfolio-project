9b866262b555a1f69fde67d30660119c1760ee51
Sumudu Hasinika <sumuduhasinika431@gmail.com>
2026-04-01 23:19:11 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+//Game link
+function openGame(event, url) {
+        event.preventDefault();
+        window.open(url, "_blank");
+}
+
+//Coffee shop  link
+function openCoffeeShop(event, url) {
+        event.preventDefault();
+        window.open(url, "_blank");
+}
+
+//1.Assignment link
+function openAssignment(event, url) {
+        event.preventDefault();
+        window.open(url, "_blank");
+}
+
+//2. Assignment link
+function openAssignment(event, url) {
+        event.preventDefault();
+        window.open(url, "_blank");
+}
+
+//3. Assignment link
+function openAssignment(event, url) {
+        event.preventDefault();
+        window.open(url, "_blank");
+}
+
+//4. Assignment link
+function openAssignment(event, url) {
+        event.preventDefault();
+        window.open(url, "_blank");
+}
+
+//5. Assignment link
+function openAssignment(event, url) {
+        event.preventDefault();
+        window.open(url, "_blank");
+}
+
+//6. Assignment link
+function openAssignment(event, url) {
+        event.preventDefault();
+        window.open(url, "_blank");
+}
+
+//7. Assignment link
+function openAssignment(event, url) {
+        event.preventDefault();
+        window.open(url, "_blank");
+}
+
+//8. Assignment link
+function openAssignment(event, url) {
+        event.preventDefault();
+        window.open(url, "_blank");
+}   
+
+//9. Assignment link
+function openAssignment(event, url) {
+        event.preventDefault();
+        window.open(url, "_blank");
+}
+
+//10. Assignment link
+function openAssignment(event, url) {
+        event.preventDefault();
+        window.open(url, "_blank");
+}   
+//11. Assignment link
+function openAssignment(event, url) {
+        event.preventDefault();
+        window.open(url, "_blank");
+}
+
+//12. Assignment link
+function openAssignment(event, url) {
+        event.preventDefault();
+        window.open(url, "_blank");
+}
+
+
+//contact
+const form = document.getElementById('contact-form');
+
+    form.addEventListener('submit', function(e) {
+        // Page eka reload wena eka nawathwanawa
+        e.preventDefault();
+
+        // 2. Alert eka pennanawa
+        alert('Your message sent!');
+
+        // 3. Form eka reset karanawa 
+        this.reset();
+    })
